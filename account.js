f849b2bf5b201da27686f6504c02d2e7c163fc88
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-10-24 12:31:46 +0100

    rejects 0 balance

--- account.js
+++ account.js
@@ -1,18 +1,24 @@
 class Account {
-    getBalance = () => {
-        let balanceString = "date || credit || debit || balance/n"
-        if (this.creditString === undefined)
-        {return balanceString}
-        else
-        {return balanceString + this.creditString}
-    };
-    addBalance = (credit) => {
-        let creditToString = credit.toString()
-        if (creditToString === creditToString.replace(/\D/g,'') && Number.isInteger(credit))
-        {this.creditString = `       ||${credit}||       ||${credit}/n`;}
-        else
-        {this.creditString = undefined};
+  getBalance = () => {
+    let balanceString = "date || credit || debit || balance/n";
+    if (this.creditString === undefined) {
+      return balanceString;
+    } else {
+      return balanceString + this.creditString;
     }
-};
+  };
+  addBalance = (credit) => {
+    let creditToString = credit.toString();
+    if (
+      creditToString === creditToString.replace(/\D/g, "") &&
+      Number.isInteger(credit) &&
+      credit > 0
+    ) {
+      this.creditString = `       ||${credit}||       ||${credit}/n`;
+    } else {
+      this.creditString = undefined;
+    }
+  };
+}
 
 module.exports = Account;
--- account.test.js
+++ account.test.js
@@ -1,38 +1,57 @@
-const Account = require('./Account');
+const Account = require("./Account");
 
-describe('Account', () => {
-    it('initially does not display a balance', () => {
-        const account = new Account;
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n")
-    });
-    it('allows a balance to be added', () => {
-        const account = new Account;
-        account.addBalance(500)
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n       ||500||       ||500/n")
-    });
-    it('allows a balance to be added', () => {
-        const account = new Account;
-        account.addBalance(10)
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n       ||10||       ||10/n")
-    });
-    it('rejects a balance that is not an integer', () => {
-        const account = new Account;
-        account.addBalance('potato')
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n")
-    });
-    it('rejects a balance that is technically an integer but not fully numeric', () => {
-        const account = new Account;
-        account.addBalance(1-4)
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n")
-    });
-    it('rejects a negative credit amount', () => {
-        const account = new Account;
-        account.addBalance(-100)
-        expect(account.getBalance()).toEqual("date || credit || debit || balance/n")
-    });
-    // it('adds a current date to the balance statement', () => {
-    //     const account = new Account;
-    //     account.addBalance(10)
-    //     expect(account.getBalance()).toEqual("date || credit || debit || balance/n       ||10||       ||10/n")
-    // });
+describe("Account", () => {
+  it("initially does not display a balance", () => {
+    const account = new Account();
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n"
+    );
+  });
+  it("allows a balance to be added", () => {
+    const account = new Account();
+    account.addBalance(500);
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n       ||500||       ||500/n"
+    );
+  });
+  it("allows a balance to be added", () => {
+    const account = new Account();
+    account.addBalance(10);
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n       ||10||       ||10/n"
+    );
+  });
+  it("rejects a balance that is not an integer", () => {
+    const account = new Account();
+    account.addBalance("potato");
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n"
+    );
+  });
+  it("rejects a balance that is technically an integer but not fully numeric", () => {
+    const account = new Account();
+    account.addBalance(1 - 4);
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n"
+    );
+  });
+  it("rejects a negative credit amount", () => {
+    const account = new Account();
+    account.addBalance(-100);
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n"
+    );
+  });
+  it("does not allow 0 to be added as credit", () => {
+    const account = new Account();
+    account.addBalance(0);
+    expect(account.getBalance()).toEqual(
+      "date || credit || debit || balance/n"
+    );
+  });
+  // it('adds a current date to the balance statement', () => {
+  //     const account = new Account;
+  //     account.addBalance(10)
+  //     expect(account.getBalance()).toEqual("date || credit || debit || balance/n       ||10||       ||10/n")
+  // });
 });
