a66a6af530b4dd8cb73674b555bf8f42572f27de
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-10-24 14:10:25 +0100

    withdrawal functionality implemented

--- account.js
+++ account.js
@@ -2,23 +2,28 @@ class Account {
     constructor() {
         this.date = new Date().toLocaleDateString()
         this.history = []
+        this.balance = 0
     }
   getBalance = () => {
     let balanceString = "date || credit || debit || balance/n";
     if (this.history.length > 0) {
-        let balance = 0
+        this.balance = 0
     this.history.forEach(transaction => {
-        balance += transaction[1]
-        if (transaction[1] > 0) {
-        balanceString += `${transaction[0]}||${transaction[1]}||        ||${balance}/n`
-    }
-    else {
-        balanceString += `${transaction[0]}||       ||${(transaction[1]*-1)}||${balance}/n`
-    }
+        this.balance += transaction[1];
+        balanceString += this.formatString(transaction);
     })}
       return balanceString;
     };
-  inputChecker = (input) => {
+formatString = (transactionArray) => {
+    if (transactionArray[1] > 0) {
+        return `${transactionArray[0]}||${transactionArray[1]}||        ||${this.balance}/n`
+    }
+    else {
+        return `${transaction[0]}||       ||${(transaction[1]*-1)}||${this.balance}/n`
+    }
+
+}
+  checkInput = (input) => {
     let inputString = input.toString();
     if (
       inputString === inputString.replace(/\D/g, "") && /*test for similarity if 'credit' includes mathematical functions*/
@@ -29,14 +34,14 @@ class Account {
     {return false} /*not allowing 0 balance to be added*/
   }
   addBalance = (amount) => {
-     if (this.inputChecker(amount) === true)
+     if (this.checkInput(amount) === true)
     {
         let transaction = [this.date, amount]
       this.history.push(transaction)
     };
   };
   withdrawBalance = (amount) => {
-    if (this.inputChecker(amount) === true) {
+    if (this.checkInput(amount) === true) {
         let withdrawal = (amount * -1)
     let transaction = [this.date, withdrawal];
     this.history.push(transaction);
