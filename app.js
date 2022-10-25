593eac27e4bf7099c33afc9c31ba132a3df6a6f3
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-10-25 16:09:36 +0100

    account passing

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const Account = require('./Account');
+const User = require('./User')
 
+clock = sinon.useFakeTimers(new Date(2023,1,10).getTime());
+const testUser = new User;
 const testAccount = new Account;
 testAccount.addBalance(1000);
-testAccount.getBalance();
-testAccount.addBalance(100);
-testAccount.getBalance();
-testAccount.withdrawBalance(200);
-testAccount.getBalance();
+clock = sinon.useFakeTimers(new Date(2023,0,13).getTime());
+testAccount.addBalance(2000);
+clock = sinon.useFakeTimers(new Date(2011,0,14).getTime());
+testAccount.withdrawBalance(500);
+testUser.addAccount(testAccount);
+testUser.getStatement();
+
