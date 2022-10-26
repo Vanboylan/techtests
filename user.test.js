83f91254184c3ac58d761845f9d176df551f6404
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-10-26 11:04:35 +0100

    final update

--- user.test.js
+++ user.test.js
@@ -1,19 +1,21 @@
-const User = require('./User');
-const Account = require('./Account');
+const User = require("./User");
+const Account = require("./Account");
 
 jest.mock("./account");
 
 beforeEach(() => {
-    mockAccount = new Account();
-    testUser = new User;
-})
+  mockAccount = new Account();
+  testUser = new User();
+});
 
-describe('user', () => {
-    it('creates a user with an empty balance', () => {
-        expect(testUser.getStatement()).toEqual("date || credit || debit || balance\n");
-    });
-    it('creates a user and allows an account to be added', () => {
-        testUser.addAccount(mockAccount);
-        expect(testUser.accounts.length).toEqual(1)
-    })
-})
+describe("user", () => {
+  it("creates a user with an empty balance", () => {
+    expect(testUser.getStatement()).toEqual(
+      "date || credit || debit || balance\n"
+    );
+  });
+  it("creates a user and allows an account to be added", () => {
+    testUser.addAccount(mockAccount);
+    expect(testUser.accounts.length).toEqual(1);
+  });
+});
